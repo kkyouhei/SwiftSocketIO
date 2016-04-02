1dcc60543c5709883ddcea91f1e6e9babffd849b
kkyouhei <hope637@gmail.com>
2016-04-02 20:33:57 +0900

    Initial Commit

---
+++ ChatSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChatSample
+//
+//  Created by 狩野 恭平 on 2016/04/02.
+//  Copyright © 2016年 狩野 恭平. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
